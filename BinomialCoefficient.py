1b05d111d34e58099c3e9deab5f47c2eab0dd594
juliusBam <80284467+juliusBam@users.noreply.github.com>
2021-03-23 14:07:33 +0100

    Add files via upload
    Short python code to calculate the binomial coefficient of 2 variables n and k.

---
+++ BinomialCoefficient.py
@@ -0,0 +1,12 @@
+import math
+n = 9
+k = 2
+if (k < 0 or k > n):
+    print('k doesn\'t have a valid value, check your inputs')
+elif (n < 0):
+    print('n doesn\'t have a valid value, check your inputs')
+elif (n == k):
+    print(f'Since n = k the binomial coefficient of {n} over {k} is: 1')
+else:
+    a = math.trunc(math.factorial(n)/(math.factorial(k) * math.factorial(n-k)))
+    print(f'The binomial coefficient of {n} over {k} is: {a}')
